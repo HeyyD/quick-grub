3058eb3088ec0da219b11c1bc4765f1793bff6a1
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-22 20:13:28 +0200

    Button styling

--- components/RecipeButton.js
+++ components/RecipeButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Image, Text, Dimensions } from 'react-native';
+import { TouchableOpacity, Image, Text, Dimensions, View } from 'react-native';
 
 import styles from './RecipeButton.scss';
 
@@ -10,9 +10,12 @@ export default class RecipeButton extends Component {
     const size = Dimensions.get('window').width / 3;
 
     return (
-      <TouchableOpacity style={{...styles['recipe-button-container'], height: size, width: size}}>
-        <Image style={styles['recipe-button-container-image']} source={{uri: this.props.img}} />
-        <Text style={styles['recipe-button-container-text']}>{this.props.label}</Text>
+      <TouchableOpacity style={{...styles['recipe-button'], height: size, width: size}}>
+        <Image style={styles['recipe-button-image']} source={{uri: this.props.img}} />
+        <View style={styles['recipe-button-overlay']}></View>
+        <View style={styles['recipe-button-text-container']}>
+          <Text style={styles['recipe-button-text']}>{this.props.label}</Text>
+        </View>
       </TouchableOpacity>
     );
   }
