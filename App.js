3765042c06b5b0c9ef1351f0a2f35a6b076c9af6
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-22 17:21:36 +0200

    Get recipes from api

--- App.js
+++ App.js
@@ -20,6 +20,25 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      APP_ID: '685bcca3',
+      APP_KEY: '2e9706babb21c1594844fe1f2d45113b'
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.edamam.com/search?q=chicken&app_id=${this.state.APP_ID}&app_key=${this.state.APP_KEY}&calories=591-722&health=alcohol-free`)
+    .then(res => res.json())
+    .then(res => {
+      res.hits.forEach(hit => {
+        console.log(hit.recipe);
+      })
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
