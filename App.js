8d7dceff15c4be7b056cf87c5f1b89039bd03b6d
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-22 18:30:04 +0200

    New component recipe list

--- App.js
+++ App.js
@@ -7,37 +7,13 @@
  */
 
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-
-import styles from './App.scss'
+import RecipeList from './components/RecipeList';
 
 type Props = {};
 export default class App extends Component<Props> {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      APP_ID: '685bcca3',
-      APP_KEY: '2e9706babb21c1594844fe1f2d45113b',
-      recipes: []
-    };
-  }
-
-  componentDidMount() {
-    fetch(`https://api.edamam.com/search?q=chicken&app_id=${this.state.APP_ID}&app_key=${this.state.APP_KEY}&calories=591-722&health=alcohol-free`)
-    .then(res => res.json())
-    .then(res => {
-      const recipes = res.hits.map(hit => hit.recipe);
-      console.log(recipes);
-      this.setState({recipes: recipes});
-    });
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        {this.state.recipes.map((recipe, index) => <Text key={index}>{recipe.label}</Text>)}
-      </View>
+      <RecipeList />
     );
   }
 }
