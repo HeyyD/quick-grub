5b84bf9cc8688817f307966c40e902f655ca66bf
HeyyD <heidi.hati@cs.tamk.fi>
2018-12-28 15:20:43 +0200

    list ingredient lines

--- components/RecipePage.js
+++ components/RecipePage.js
@@ -5,6 +5,8 @@ import data from './tempData.json';
 
 export default class RecipePage extends Component {
 
+  data;
+
   static navigationOptions = {
     title: 'Recipe'
   }
@@ -13,14 +15,25 @@ export default class RecipePage extends Component {
     super(props);
     // const data = this.props.navigation.state.params.data;
     console.log(data);
+    this.data = data;
   }
 
   render() {
     return (
-      <View>
+      <View style={styles['recipe']}>
         <View style={styles['recipe-background-container']}>
-          <Image style={styles['recipe-background-image']} source={ {uri: data.image} } />
+          <Image style={styles['recipe-background-image']} source={ {uri: this.data.image} } />
           <View style={styles['recipe-background-overlay']}></View>
+        </View>
+        <View style={styles['recipe-content']}>
+          <Text style={styles['recipe-content-title']}>{this.data.label}</Text>
+          <View style={styles['recipe-ingredients']}>
+            {
+              this.data.ingredientLines.map((line, index) => {
+                return <Text style={styles['recipe-ingredients-line']} key={index}>{line}</Text>;
+              })
+            }
+          </View>
         </View>
       </View>
     );
