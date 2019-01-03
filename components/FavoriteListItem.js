d4a4eef50a3c66d9e8e684d684f552f8513c5681
HeyyD <heidi.hati@cs.tamk.fi>
2019-01-03 18:38:29 +0200

    Icons and list item styles

--- components/FavoriteListItem.js
+++ components/FavoriteListItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import styles from './FavoriteListItem.scss';
 
 export default class FavoriteListItem extends Component {
   render() {
     return(
-      <Text>{ this.props.data.label }</Text>
+      <View style={ styles['list-item-container'] }>
+        <Text style={ styles['list-item-label'] }>{ this.props.data.label }</Text>
+        <TouchableOpacity style={ styles['list-item-menu'] }>
+          <Icon style={ styles['list-item-menu-icon'] } color='gray' name='ellipsis-v' size={25} />
+        </TouchableOpacity>
+      </View>
     );
   }
 }
